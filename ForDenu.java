e29d0271539601f580531b3343cbe504aa983e2d
GoDyPs <354052237@qq.com>
2017-05-14 14:10:36 +0800

    梯形星星
    输出梯形的星星

---
+++ ForDenu.java
@@ -0,0 +1,11 @@
+class ForDenu{
+	public static void main(String[ ] args){
+		for(int i=0;i<5;i++){
+			for(int x=0;x<=i;x++){
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+}
